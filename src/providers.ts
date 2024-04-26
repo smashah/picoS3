247c251b5d6a0c47db1cc3fcd64786235f82ffa2
M SHAH <shah@idk.uno>
2024-04-26 03:41:18 +0300

    ✨ added R2 provider, supabase doesnt work

---
+++ src/providers.ts
@@ -0,0 +1,60 @@
+import { SERVICE_PROVIDER_CONFIG } from "./types"
+import { resolvePath } from "./utils"
+
+export enum CLOUD_PROVIDERS {
+    GCP = "GCP",
+    WASABI = "WASABI",
+    AWS = "AWS",
+    CONTABO = "CONTABO",
+    DO = "DO",
+    MINIO = "MINIO",
+    R2 = "R2"
+}
+
+/**
+ * Please submit a new issue if you need another s3 compatible provider.
+ */
+export const PROVIDERS : {
+    [provierName: string]: SERVICE_PROVIDER_CONFIG
+ } = {
+    "GCP": {
+        host: ({ bucket }: any) => `${bucket}.storage.googleapis.com`,
+        url: ({ bucket, filename, directory }: any) => `https://${bucket}.storage.googleapis.com/${resolvePath({filename, directory})}`,
+        res: ({ bucket, filename, directory }: any) => `https://storage.googleapis.com/${bucket}/${resolvePath({filename, directory})}`
+    },
+    "AWS": {
+        host: ({ bucket }: any) => `${bucket}.s3.amazonaws.com`,
+        url: ({ bucket, filename, directory }: any) => `https://${bucket}.s3.amazonaws.com/${resolvePath({filename, directory})}`,
+        res: ({ bucket, filename, directory, region }: any) => `https://${bucket}.s3.${region}.amazonaws.com/${resolvePath({filename, directory})}`
+    },
+    "WASABI": {
+        host: ({ region, bucket }: any) => `${bucket}.s3.${region}.wasabisys.com`,
+        url: ({ region, filename, directory, bucket }: any) => `https://${bucket}.s3.${region}.wasabisys.com/${resolvePath({filename, directory})}`,
+        res: ({ bucket, filename, directory, region }: any) => `https://s3.${region}.wasabisys.com/${bucket}/${resolvePath({filename, directory})}`
+    },
+    "DO": {
+        host: ({ region, bucket }: any) => `${bucket}.${region}.digitaloceanspaces.com`,
+        url: ({ bucket, filename, directory, region }: any) => `https://${bucket}.${region}.digitaloceanspaces.com/${resolvePath({filename, directory: `${directory}`})}`,
+        res: ({ bucket, filename, directory, region }: any) => `https://${bucket}.${region}.digitaloceanspaces.com/${resolvePath({filename, directory: `${directory}`})}`
+    },
+    "CONTABO": {
+        host: ({ region }: any) => `${region}.contabostorage.com`,
+        url: ({ bucket, filename, directory, region }: any) => `https://${region}.contabostorage.com/${resolvePath({filename, directory: `${bucket}${directory}`})}`,
+        res: ({ bucket, filename, directory, region }: any) => `https://${region}.contabostorage.com/${resolvePath({filename, directory: `${bucket}${directory}`})}`
+    },
+    "MINIO": {
+        host: ({ host }: any) => `${host}`,
+        url: ({ bucket, filename, directory, host }: any) => `${host}/${bucket}/${resolvePath({filename, directory: `${directory}`})}`,
+        res: ({ bucket, filename, directory, host }: any) => `${host}/${bucket}/${resolvePath({filename, directory: `${directory}`})}`
+    },
+    "SUPABASE": {
+        host: ({ host }: any) => `${host}`,
+        url: ({ bucket, filename, directory, host }: any) => `${host}/storage/v1/s3/${bucket}/${resolvePath({filename, directory: `${directory}`})}`,
+        res: ({ bucket, filename, directory, host }: any) => `${host}/storage/v1/s3/${bucket}/${resolvePath({filename, directory: `${directory}`})}`
+    },
+    "R2": {
+        host: ({ host, bucket }: any) => `https://${bucket}.${host.replace('https://',"")}`,
+        url: ({ bucket, filename, directory, host }: any) => `https://${bucket}.${host.replace('https://',"")}/${resolvePath({filename, directory: `${directory}`})}`,
+        res: ({ bucket, filename, directory, host }: any) => `https://${bucket}.${host.replace('https://',"")}/${resolvePath({filename, directory: `${directory}`})}`
+    }
+}
